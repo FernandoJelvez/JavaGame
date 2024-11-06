c8ee2e91e92edf7669af7fc4954531f7f9b398cb
FernandoJelvez <f.jelvez02@ufromail.cl>
2024-11-06 10:12:20 -0300

    Add files via upload

---
+++ javaGame/core/src/main/java/io/github/javaGame/Controllable.java
@@ -0,0 +1,11 @@
+package org.example.workpaths.two.engine;
+
+public interface Controllable {
+	void up();
+	void down();
+	void left();
+	void right();
+	void primaryButton();
+	void secondaryButton();
+	void menuButton();
+}
