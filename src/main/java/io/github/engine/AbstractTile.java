9bcf4b4f6fd30833a002a504fa3d4fe1d05f3abc
FernandoJelvez <f.jelvez02@ufromail.cl>
2024-11-20 18:18:53 -0300

    Update AbstractTile.java

--- src/main/java/io/github/engine/AbstractTile.java
+++ src/main/java/io/github/engine/AbstractTile.java
@@ -3,52 +3,36 @@ package io.github.engine;
 
 import javax.swing.JPanel;
 import java.awt.Dimension;
-import java.awt.Color;
 import java.awt.Point;
 
-public abstract class AbstractTile extends JPanel implements Comparable<AbstractTile> {
-	private int layer=0;
-	private Color color;
-	//int x,y,width,height;
-	public AbstractTile(int width, int height,String id){
-		super();
+public abstract class AbstractTile extends JPanel {
+	private boolean solid;
+	private int layer;
+	public AbstractTile(int width, int height,boolean solid,int layer){
 		super.setSize(width,height);
+		this.solid=solid;
+		this.layer=layer;
 	}
-	public AbstractTile(int x, int y, int width, int height,String id){
-		super();
+
+	public AbstractTile(int x, int y, int width, int height, boolean solid,int layer){
 		super.setSize(width,height);
 		super.setLocation(x,y);
+		this.solid=solid;
+		this.layer=layer;
 	}
 
-	public AbstractTile(int x, int y, Dimension d,String id) {
-		super();
+	public AbstractTile(Point p, Dimension d, boolean solid,int layer) {
 		super.setSize(d);
-	}
-	public AbstractTile(Point p, Dimension d,String id) {
-		super();
-		super.setSize(d);
-	}
-
-	/**
-	 * sets the drawing level of the Tile
-	 * @param layer is the new drawing level for the object, a greater level will be displayed on top of a
-	 * Tile object with a smaller drawing level
-	 */
-	public void setLayer(int layer){
-		if(layer>10||layer<0){
-			throw new ArithmeticException("Layer value must be a value from 0 to 10");
-		} else {
-			this.layer =layer;
-		}
+		super.setLocation(p);
+		this.solid=solid;
+		this.layer=layer;
 	}
 
-	public int getLayer(){
+	public boolean isSolid() {
+		return solid;
+	}
+
+	public int getLayer() {
 		return layer;
-	}
-
-	@Deprecated
-	@Override
-	public int compareTo(AbstractTile tile) {
-		return layer - tile.getLayer();
 	}
 }
