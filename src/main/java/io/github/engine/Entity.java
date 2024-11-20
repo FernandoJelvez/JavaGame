bfe08dac08039f2f7d67bab6c637701d79008520
FernandoJelvez <f.jelvez02@ufromail.cl>
2024-11-20 18:23:22 -0300

    Update Entity.java

--- src/main/java/io/github/engine/Entity.java
+++ src/main/java/io/github/engine/Entity.java
@@ -1,49 +1,63 @@
 package io.github.engine;
 
 public abstract class Entity extends AbstractTile {
-	private long airTime;
-	private int xSpeed,ySpeed = 0;
+	private double airTime;
+	private double xSpeed,ySpeed;
 	private int gravity;//the gravity the entity uses if it doesn't use the global gravity
-	private boolean globalGravity; //signals if the entity uses the default gravity(true) or its own(false)
+	private boolean usingGlobalGravity; //signals if the entity uses the default gravity(true) or its own(false)
 
-
-	/*
-	*
-	*
-	*/
-	public Entity(int x, int y, AbstractTile tile, boolean globalGravity,String id){
-		super(x,y,tile.getSize(),id);
-		this.globalGravity = globalGravity;
+	public Entity(int x, int y, AbstractTile tile, boolean usingGlobalGravity){
+		super(x,y,tile.isSolid(),tile.getLayer());
+		this.usingGlobalGravity = usingGlobalGravity;
 	}
 
-	public Entity(int x, int y, AbstractTile tile,String id){
-		super(x,y,tile.getSize(),id);
-		this.globalGravity = true;
-	}
-	public Entity(int x, int y, int width,int height,String id){
-		super(x,y,width,height,id);
-		this.globalGravity = true;
+	public Entity(int x, int y, AbstractTile tile){
+		super(x,y,tile.getWidth(),tile.getHeight(),tile.isSolid(),tile.getLayer());
+		this.usingGlobalGravity = true;
+		ySpeed=0;
 	}
 
-	public boolean isGlobalGravity() {
-		return globalGravity;
+	public Entity(int x, int y, int width,int height,boolean solid,int layer){
+		super(x,y,width,height,solid,layer);
+		this.usingGlobalGravity = true;
+		ySpeed=0;
 	}
-	public int getySpeed(){
+
+	/**
+	 * Updates the position of the object in screen, both horizontally and vertically
+	 * @apiNote this method should be overrode by any subclass that needs a different movement behavior
+	 */
+	public void refresh(){
+		int y =(int)(ySpeed*airTime);
+		setLocation(getX()+(int)(xSpeed*DisplayRefresh.getDeltaTime()),y);
+		airTime += DisplayRefresh.getDeltaTime();
+	}
+
+	public boolean isUsingGlobalGravity() {
+		return usingGlobalGravity;
+	}
+
+	public void setUsingGlobalGravity(boolean flag){
+		this.usingGlobalGravity=flag;
+	}
+
+	public void setGravity(int gravity) {
+		this.gravity = gravity;
+	}
+
+	public double getYSpeed(){
 		return ySpeed;
 	}
-	public void setySpeed(int ySpeed){
+
+	public void setYSpeed(double ySpeed){
 		this.ySpeed=ySpeed;
 	}
-	public long getAirTime(){
+
+	public double getAirTime(){
 		return airTime;
 	}
-	public void updatePosition(){
-		System.out.println("Y"+super.getY());
-		System.out.println("Speed*air time"+(ySpeed*airTime));
-		System.out.println("nuevo Y ideal "+(super.getY()+ySpeed*airTime));
-		super.setLocation(super.getX(), (int) (super.getY()+ySpeed*airTime));
-		airTime+=UpdateManager.getDeltaTime();
 
-		System.out.println("air time "+airTime);
+	public void setXSpeed(double xSpeed) {
+		this.xSpeed = xSpeed;
 	}
 }
