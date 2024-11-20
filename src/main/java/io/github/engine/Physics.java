9e8fb2752ef5d7bdba20100f5ddcdb16eabb1fe7
FernandoJelvez <f.jelvez02@ufromail.cl>
2024-11-20 18:23:53 -0300

    Update Physics.java

--- src/main/java/io/github/engine/Physics.java
+++ src/main/java/io/github/engine/Physics.java
@@ -4,40 +4,32 @@ package io.github.engine;
  *Management of the interactions between game entities and their surroundings
  */
 public final class Physics {
-    private static int globalGravity; /*global gravity, the gravity that objects follow by default,
-								 if not set otherwise by the entity*/
-
-	/**
-	 *Receives the vertical speed of an Entity and the time it has been on the air, and
-	 * returns the new vertical speed according to the effect of gravity
-	 *@param ySpeed The vertical speed of an Entity
-	 *@param airTime The time that the Entity has spend in the air from the last time
-	 *it touched a floor
-	 */
-    public static int applyGravity(int ySpeed,int airTime){
-        ySpeed=ySpeed+globalGravity *airTime;
-        return ySpeed;
-    }
+    private static double globalGravity;
 
 	/**
 	 *Applies gravity to an Entity, changing its vertical speed
 	 * @param entity The Entity upon which gravity is applied
 	 */
 	public static void applyGravity(Entity entity){
-		entity.setySpeed((int) (entity.getySpeed()+globalGravity *entity.getAirTime()));
-		entity.updatePosition();
+		double newYSpeed = (entity.getYSpeed()+((globalGravity * Display.getHeigth()) /2000) *entity.getAirTime());
+		entity.setYSpeed(newYSpeed);
+		entity.refresh();
 	}
 
-	public static void setGlobalGravity(int gravity) {
+	public static void setGlobalGravity(double gravity) {
 		globalGravity = gravity;
 	}
+	public static void checkFreePath(){
+		//TODO: build the path checker
+		//TODO: check if this method must go in Entity instead
+		//checks if an Entity can Travel one more step before colliding
+		//if it cant, then the path is shortened to the more distant point the Entity can go without colliding
+	}
 
-	//public boolean checkCollition(Entity entity, Tile tile){
-	//	ArrayList<Integer> i = new ArrayList<>();
-	//}
-
-	public static boolean checkCollition(int xPos, int yPos, int width, int height){
-		//TODO: build the collition system
-		return true;
+	public static boolean checkColliding(AbstractTile tile){
+		return false;
+	}
+	private static void callCollition(AbstractTile Tile1, AbstractTile Tile2){
+		//TODO: add collition calling, this method must decide if the collition is top or side collition
 	}
 }
