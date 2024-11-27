704b092816cbaa755ddb60c9d4413d80385384dd
FernandoJelvez <f.jelvez02@ufromail.cl>
2024-11-27 19:09:30 -0300

    Update AbstractTile.java, change of architecture
    The AbstractTile class no longer extends JPanel, instead it uses an object of the JLabel class, this encapsulates the swing components and methods within the engine package and allows for more control of the methods and functions being implemented.
    In the case of the change from JPanel to JLabel, it was made due to convenience

--- src/main/java/io/github/engine/AbstractTile.java
+++ src/main/java/io/github/engine/AbstractTile.java
@@ -1,38 +1,84 @@
 package io.github.engine;
 
 
-import javax.swing.JPanel;
-import java.awt.Dimension;
-import java.awt.Point;
+import javax.swing.JLabel;
+import java.awt.Color;
 
-public abstract class AbstractTile extends JPanel {
+public abstract class AbstractTile {
+	private final JLabel = new JLabel();
 	private boolean solid;
 	private int layer;
+	private int unitHeight;
+	private int unitWidth;
+	
 	public AbstractTile(int width, int height,boolean solid,int layer){
-		super.setSize(width,height);
+		label.setSize(width*Display.getUnitValue(),height*Display.getUnitValue());
+		unitWidth=width;
+		unitHeight=height;
 		this.solid=solid;
 		this.layer=layer;
+		label.setOpaque(true);
 	}
-
-	public AbstractTile(int x, int y, int width, int height, boolean solid,int layer){
-		super.setSize(width,height);
-		super.setLocation(x,y);
-		this.solid=solid;
-		this.layer=layer;
-	}
-
-	public AbstractTile(Point p, Dimension d, boolean solid,int layer) {
-		super.setSize(d);
-		super.setLocation(p);
-		this.solid=solid;
-		this.layer=layer;
-	}
-
+	
 	public boolean isSolid() {
 		return solid;
 	}
 
+	public void setSolid(boolean flag){
+		solid=flag;
+	}
+	
 	public int getLayer() {
 		return layer;
 	}
+	public void setSize(int width,int height) {
+		label.setSize(width*Display.getUnitValue(), height*Display.getUnitValue());
+		unitWidth=width;
+		unitHeight=height;
+	}
+
+	public int getWidth(){
+		return (int)(label.getSize().getWidth()/Display.getUnitValue());
+	}
+	public int getHeight(){
+		return (int)(label.getSize().getHeight()/Display.getUnitValue());
+	}
+	public void setLocation(float x, float y){
+		label.setLocation(Math.round(x*Display.getUnitValue()),Math.round((y-getHeight())*Display.getUnitValue()));
+	}
+	public void setVisible(boolean flag){
+		label.setVisible(flag);
+	}
+	public boolean isVisible(){
+		return label.isVisible();
+	}
+
+	/**
+	 * This method returns the x coordinate of the bottom left corner of the tile
+	 * @return the x coordinate in units
+	 */
+	public float getX(){
+		return (float) label.getX() /Display.getUnitValue();
+	}
+
+	/**
+	 * This method returns the y coordinate of the bottom left corner of the tile
+	 * @return the y coordinate in units
+	 */
+	public float getY(){
+		return (float) (label.getBounds().getMaxY()/Display.getUnitValue());
+	}
+	public void setColor(int r,int g,int b){
+		label.setBackground(new Color(r,g,b));
+		System.out.println(new Color(r,g,b));
+	}
+
+	public void refreshSize(){
+		setSize(unitWidth,unitHeight);
+	}
+	/**
+	*This method is called by the {@code Display} class every frame, it should be used
+ 	*with the purpose of changing size, color or any other refresh application
+	*/
+	public abstract void refresh();
 }
