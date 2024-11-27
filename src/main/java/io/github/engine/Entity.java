f090d2d855e708593e90e2dd0a10b7fe9fd36d9a
FernandoJelvez <f.jelvez02@ufromail.cl>
2024-11-27 20:42:30 -0300

    Update Entity.java
    Change usingGlobalGravity to usingLocalGravity
    Removed getAirTime method, as it is no longer used by the physics class.
    Added getGravity method
    Changed setGlobalGravity to changeLocalGravity

--- src/main/java/io/github/engine/Entity.java
+++ src/main/java/io/github/engine/Entity.java
@@ -4,12 +4,7 @@ public abstract class Entity extends AbstractTile {
 	private double airTime;
 	private double xSpeed,ySpeed;
 	private int gravity;//the gravity the entity uses if it doesn't use the global gravity
-	private boolean usingGlobalGravity; //signals if the entity uses the default gravity(true) or its own(false)
-
-	public Entity(int x, int y, AbstractTile tile, boolean usingGlobalGravity){
-		super(x,y,tile.isSolid(),tile.getLayer());
-		this.usingGlobalGravity = usingGlobalGravity;
-	}
+	private boolean usingLocalGravity=false; //signals if the entity uses the default gravity(false) or its own(true)
 
 	public Entity(int x, int y, AbstractTile tile){
 		super(x,y,tile.getWidth(),tile.getHeight(),tile.isSolid(),tile.getLayer());
@@ -22,42 +17,33 @@ public abstract class Entity extends AbstractTile {
 		this.usingGlobalGravity = true;
 		ySpeed=0;
 	}
-
-	/**
-	 * Updates the position of the object in screen, both horizontally and vertically
-	 * @apiNote this method should be overrode by any subclass that needs a different movement behavior
-	 */
-	public void refresh(){
-		int y =(int)(ySpeed*airTime);
-		setLocation(getX()+(int)(xSpeed*DisplayRefresh.getDeltaTime()),y);
-		airTime += DisplayRefresh.getDeltaTime();
-	}
-
+	
 	public boolean isUsingGlobalGravity() {
 		return usingGlobalGravity;
 	}
 
-	public void setUsingGlobalGravity(boolean flag){
-		this.usingGlobalGravity=flag;
+	public void setUsingLocalGravity(boolean flag){
+		this.usinglocalGravity=flag;
 	}
 
-	public void setGravity(int gravity) {
+	public void changeLocalGravity(int gravity) {
 		this.gravity = gravity;
 	}
 
 	public double getYSpeed(){
 		return ySpeed;
 	}
+	public double getXSpeed() {
+		return xSpeed;
+	}
 
 	public void setYSpeed(double ySpeed){
 		this.ySpeed=ySpeed;
 	}
-
-	public double getAirTime(){
-		return airTime;
-	}
-
 	public void setXSpeed(double xSpeed) {
 		this.xSpeed = xSpeed;
+	}
+	public int getGravity() {
+		return gravity;
 	}
 }
