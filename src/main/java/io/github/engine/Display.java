5733f4a13adc9e7a5eb04bf54a63c2d86c71f0bb
FernandoJelvez <f.jelvez02@ufromail.cl>
2024-11-27 20:18:14 -0300

    Update Display.java
    Added the refreshSize method, which is in charge of changing the size of the elements on screen when the screen size is changed.
    Added the getUnitValue method, which returns a value corresponding the the height of the window divided between 60, and it is used as the standard measurement unit
    Added the checkScreenSizeChanged Method, and the screenHeight to check if the screen height was changed and, if it was changed, it calls refreshSize
    Added a JPanel due to changes made in the AbstractTile class, wich now uses a JLabel object

--- src/main/java/io/github/engine/Display.java
+++ src/main/java/io/github/engine/Display.java
@@ -12,9 +12,11 @@ import java.util.HashMap;
  */
 public final class Display {
     private static JFrame frame;
+    private static JPanel panel;
     private static HashMap<String,AbstractTile> buffer=new HashMap<>();
     private static HashMap<String, AbstractTile> tiles =new HashMap<>();
     private static boolean bufferChanged;
+    private static int screenHeight
 
     /**
      * This method sets the value of the variables of this class, in this case, it sets the
@@ -25,7 +27,11 @@ public final class Display {
      */
     public static void setup(String name,int width, int height){
         frame = new JFrame(name);
+        panel = new JPanel()
         frame.setSize(width,height);
+        panel.setSize(width,height);
+        Display.screenHeight = height;
+        frame.add(panel);
     }
 
     /**
@@ -34,6 +40,8 @@ public final class Display {
      */
     public static void start(){
         Display.frame.setVisible(true);
+        Display.panel.setVisible(true);
+        Display.frame.setLayout(null);
         Display.frame.setLayout(null);
         Display.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
@@ -49,16 +57,23 @@ public final class Display {
     }
 
     public static void refresh(){
+        if(checkScreenHeightChanged()){
+            System.out.println(true);
+            refreshSize();
+        }
         if (bufferChanged){
-            buffer.entrySet().stream().sorted()
-                    .filter((e)->!tiles.containsKey(e.getKey()))
-                    .forEach((e)->{
-                        tiles.put(e.getKey(),e.getValue()); e.getValue().setVisible(true);frame.add(e.getValue());});
+            buffer.entrySet().stream()
+                    .filter((e)->!tiles.containsKey(e.getKey())).forEach((e)->{
+                        tiles.put(e.getKey(),e.getValue()); 
+                        e.getValue().setVisible(true);
+                        panel.add(e.getValue().getLabel());
+                    });
             buffer.clear();
         }
         tiles.values().stream()
                 .filter(Entity.class::isInstance)
                 .forEach((e)->{Physics.applyGravity((Entity)e);((Entity)e).refresh();});
+        panel.revalidate();
         frame.repaint();
         bufferChanged=false;
     }
@@ -71,5 +86,23 @@ public final class Display {
     }
     public static AbstractTile retrieveTile(String id){
         return tiles.get(id);
+    }
+    /**
+     * Calculates the value of a unit, which is the an amount of pixels used as measurement by the other classes as the
+     * standard measurement unit
+     * @return an integer with the value of a unit measured in pixels
+     */
+    public static int getUnitValue(){
+		return frame.getHeight() / 60;
+    }
+    public static boolean checkScreenHeightChanged(){
+        System.out.println("check: "+ (frame.getHeight()==screenHeight));
+        System.out.println("check: "+frame.getHeight());
+        return !(frame.getHeight()==screenHeight);
+    }
+    public static void refreshSize(){
+        panel.setSize(frame.getSize());
+        screenHeight= frame.getHeight();
+        tiles.values().forEach(AbstractTile::refreshSize);
     }
 }
