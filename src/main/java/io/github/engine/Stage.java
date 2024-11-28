9a890bcc48856ce5a25755a1da8d68a322f9fcd3
FernandoJelvez <f.jelvez02@ufromail.cl>
2024-11-27 21:15:07 -0300

    Add files via upload
    Added the Stage class, which stores the game level/stage layout and tiles

---
+++ src/main/java/io/github/engine/Stage.java
@@ -0,0 +1,40 @@
+package io.github.engine;
+
+import io.github.game.Tile;
+import java.util.HashMap;
+
+public class Stage {
+	HashMap<String, Entity> entities =new HashMap<>();
+	HashMap<String, Tile> tiles=new HashMap<>();
+	//width and height in units
+	int width;
+	int height;
+
+	public Stage(int width,int height){
+		this.width=width;
+		this.height=height;
+	}
+
+	public void addEntity(Entity entity,String id){
+		entities.put(id,entity);
+	}
+	public void addTile(Tile tile,String id){
+		tiles.put(id,tile);
+	}
+
+	public HashMap<String,Entity> getEntitiesBetween(int initialX,int initialY, int finalX,int finalY){
+		HashMap<String, Entity> localEntities=new HashMap<>();
+		entities.entrySet().stream().filter((e)->e.getValue().getX()<finalX && e.getValue().getX()>initialX)
+				.filter((e)->e.getValue().getY()<finalY && e.getValue().getY()>initialY)
+				.forEach((e)->localEntities.put(e.getKey(),e.getValue()));
+		return localEntities;
+	}
+
+	public HashMap<String, Tile> getTilesBetween(int initialX,int initialY,int finalX,int finalY){
+		HashMap<String, Tile> localTiles=new HashMap<>();
+		tiles.entrySet().stream().filter((e)->e.getValue().getX()<finalX && e.getValue().getX()>initialX)
+				.filter((e)->e.getValue().getY()<finalY && e.getValue().getY()>initialY)
+				.forEach((e)->localTiles.put(e.getKey(),e.getValue()));
+		return localTiles;
+	}
+}
