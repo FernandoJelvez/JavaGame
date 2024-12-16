7a00d25cc5d1481bbe7b4b7092da40960a424e92
FernandoJelvez <f.jelvez02@ufromail.cl>
2024-12-15 21:17:20 -0300

    Add files via upload

--- src/main/java/io/github/game/Main.java
+++ src/main/java/io/github/game/Main.java
@@ -8,26 +8,22 @@ import io.github.engine.Song;
 public class Main {
 	public static void main(String[] args) {
 		Display.setup("gameTest",800,600);
-		Display.start();
-		Physics.setGlobalGravity(15);
-		Player player = new Player(5,20,1,1,true,0);
+		Player player = new Player(5,20,2,3,true,0);
 		player.setColor(0,0,0);
-		player.setYSpeed(-20);
-		player.setXSpeed(0);
+		Display.setPlayer(player);
+		Display.start();
+		Physics.setGlobalGravity(8);
+		player.setXSpeed(8);
 		Display.addToBuffer(player,"Player_1");
-		Tile tile = new Tile(2,10,10,2,true,0);
-		Tile tile2 = new Tile(12,10,2,20,true,0);
-		Tile tile3 = new Tile(2,30,10,2,true,0);
+		Tile tile = new Tile(2,30,10,2,true,0);
+		Tile tile2 = new Tile(0,50,40,10,true,0);
+		Tile tile3 = new Tile(2,45,40,2,true,0);
 		tile.setColor(255,0,0);
 		tile2.setColor(255,0,0);
 		tile3.setColor(255,0,0);
 		Display.addToBuffer(tile,"Block");
 		Display.addToBuffer(tile2,"Block_2");
 		Display.addToBuffer(tile3,"Block_3");
-		Song song = new Song("C:\\Users\\Alumno\\Downloads\\wow2.wav");
-		song.setVolumePercentage(100);
-		song.loop();
-		song.start();
 		DisplayRefresh.startClock(60);
 	}
 }
