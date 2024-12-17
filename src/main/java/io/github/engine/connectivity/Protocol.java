2d945658fe594957534fef621c73b70830c90fba
FernandoJelvez <f.jelvez02@ufromail.cl>
2024-12-17 00:32:13 -0300

    Update connectivity folder contents

--- src/main/java/io/github/engine/connectivity/Protocol.java
+++ src/main/java/io/github/engine/connectivity/Protocol.java
@@ -1,4 +1,16 @@
 package io.github.engine.connectivity;
 
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
 public interface Protocol {
+	/**
+	 * General method for the protocol interface, it is supposed to process a request made to a socket and return
+	 * the right answer to be sent back
+	 * @param input an {@code InputStream}
+	 * @return an {@code OutputStream}
+	 */
+	ObjectOutputStream processRequest(ObjectInputStream input);
 }
