e7128f19812169307bfea8a62d7bbd1566c9894a
TomasJaures <t.jaures01@ufromail.cl>
2024-12-20 16:43:31 -0400

    Codigo "Texture" modificado
    Cambiado a un codigo mas completo

--- src/main/java/io/github/engine/Texture.java
+++ src/main/java/io/github/engine/Texture.java
@@ -1,26 +1,50 @@
 package io.github.engine;
 
-import javax.imageio.ImageReader;
-import javax.imageio.stream.ImageInputStreamImpl;
 import javax.swing.*;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
-public class Texture {
-	ImageIcon texture;
-	int spriteWidth;
-	int spriteHeight;
+public class Texture implements Cloneable{
 
-	/**
-	 * Class constructor, the width and height values are used by the class to cut the image by the specified dimensions to
-	 * obtain the sprites, or specific textures, which allows more than one texture per image
-	 * @param filePath     the path of the image file
-	 * @param spriteWidth  the amount of pixels that each sprite uses in the image horizontally
-	 * @param spriteHeight the amount of pixels that each sprite uses in the image vertically
-	 */
-	public Texture(String filePath, int spriteWidth, int spriteHeight) {
-		texture = new ImageIcon(filePath);
-		this.spriteWidth = spriteWidth;
-		this.spriteHeight = spriteHeight;
+	private ImageIcon imageIcon;
+
+	public Texture(String path) {
+		imageIcon = new ImageIcon(path);
 	}
+
+	public Texture() {
+	}
+
+	public Texture(ImageIcon imageIcon) {
+		this.imageIcon = imageIcon;
+	}
+
+	public void escalarImagen(float width, float height){
+		this.imageIcon.setImage(imageIcon.getImage().getScaledInstance((int) width, (int) height, Image.SCALE_FAST));
+
+	}
+
+	protected ImageIcon getImageIcon() {
+		Image imagenCopia = this.imageIcon.getImage();
+		BufferedImage bufferedImage = new BufferedImage(imagenCopia.getWidth(null),imagenCopia.getHeight(null),BufferedImage.TYPE_INT_ARGB);
+		ImageIcon imagenSuelta = new ImageIcon(bufferedImage);
+		Graphics2D g2d = bufferedImage.createGraphics();
+		g2d.drawImage(imagenCopia,0,0,null);
+		g2d.dispose();
+
+		return imagenSuelta;
+	}
+
+
+    @Override
+    public Texture clone() {
+        try {
+            Texture clone = (Texture) super.clone();
+			clone.imageIcon = new ImageIcon(imageIcon.getImage());
+			// TODO: copy mutable state here, so the clone can't change the internals of the original
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
 }
