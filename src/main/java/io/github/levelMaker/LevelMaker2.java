f909c30cc7e3d2845a4f27f007547f3c9c58014a
TomasJaures <t.jaures01@ufromail.cl>
2024-12-31 18:52:56 -0400

    Update LevelMaker2.java
    Codigo actualizado

--- src/main/java/io/github/levelMaker/LevelMaker2.java
+++ src/main/java/io/github/levelMaker/LevelMaker2.java
@@ -3,7 +3,7 @@ package io.github.levelMaker;
 import io.github.engine.Display;
 import io.github.engine.DisplayRefresh;
 import io.github.engine.Texture;
-import io.github.engine.Player;
+import io.github.game.Player;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -13,25 +13,32 @@ import java.io.IOException;
 
 public class LevelMaker2 {
 
-    public static void main(String[] args) {
+    public static boolean isStarter = false;
+    public static int idBloque = 2;
+    public static Cursor cursor;
+    public static JLabel label = new JLabel();
 
+    public static void main(String[] args) throws IOException {
         Display.setup("goal",800,600);
-
-
-        Cursor cursor = new Cursor(0,0,4,4,false,-2);
-        cursor.setTexture(new Texture("C:\\Users\\Tomas\\OneDrive\\Desktop\\codigoEnTeoriaFinal\\JavaGame-main\\src\\main\\java\\io\\github\\presets\\img.png"));
-        Display.setControl(new Control(cursor));
-
+        Mundo.setIdPantalla(0);
+        isStarter = true;
+        cursor = new Cursor(0,0,4,4,false,2);
+        cursor.setTexture(new Texture(Mundo.asignarTexturaAlBloque(1)));
+        Display.setPlayer(cursor);
         //Tile fondoTextura = new Tile((int) Display.getWidth(), (int) Display.getHeight(), false,-1);
         //fondoTextura.setTexture(new Texture("C:\\Users\\Tomas\\OneDrive\\Desktop\\codigoEnTeoriaFinal\\JavaGame-main\\src\\main\\java\\io\\github\\presets\\FondoCreadorDeNivelesNuevo.png"));
-
         Display.addToBuffer(cursor, "cursor");
         //Display.addToBuffer(fondoTextura, "fondo");
         Display.start();
-
         DisplayRefresh.startClock(30);
 
-
+        label.setLayout(null);
+        label.setOpaque(true);
+        label.setBounds(0,0,30,30);
+        label.setText("L=0");
+        Display.panel.add(label);
     }
+
+
 
 }
