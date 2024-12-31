83b570c9c0aa8a5a580c859d22401a09165b9a44
TomasJaures <t.jaures01@ufromail.cl>
2024-12-31 18:55:59 -0400

    Update ExtendedControl.java
    Codigo actualizado para el nuevo LevelMaker2

--- src/main/java/io/github/levelMaker/ExtendedControl.java
+++ src/main/java/io/github/levelMaker/ExtendedControl.java
@@ -5,6 +5,9 @@ import io.github.engine.Controllable;
 
 import java.awt.event.KeyEvent;
 
+import static io.github.levelMaker.ExtendedControlNames.chunkDerecha;
+import static io.github.levelMaker.ExtendedControlNames.chunkIzquierda;
+
 public class ExtendedControl extends Control {
 
 
@@ -14,11 +17,26 @@ public class ExtendedControl extends Control {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        super.keyPressed(e);
+        ExtendedControlInterface controllable = (ExtendedControlInterface) getControllable();
+
+        if ((e.getKeyCode() <= 40 && e.getKeyCode() >= 37) || ( e.getKeyCode() == 88 || e.getKeyCode() == 90) ){
+            super.keyPressed(e);
+        } else {
+            switch (e.getKeyCode()){
+                case KeyEvent.VK_K:
+                    controllable.pressExtended(chunkIzquierda);
+                    System.out.println("izq");
+                    if (Mundo.getIdPantalla() > -1){
+                        Mundo.setIdPantalla(Mundo.getIdPantalla()-1);
+                    }
+                case KeyEvent.VK_E:
+                    controllable.pressExtended(chunkDerecha);
+            }
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        super.keyReleased(e);
+
     }
 }
