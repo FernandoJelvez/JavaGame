ca850ee6e730364405ce2393fdacb98a5920cbda
FernandoJelvez <f.jelvez02@ufromail.cl>
2025-01-03 19:42:57 -0300

    Delete src/main/java/io/github/gameMakerTool directory
    Remove redundant directory and Classes

--- src/main/java/io/github/gameMakerTool/AbstractExceptionManager.java
+++
@@ -1,5 +0,0 @@
-package io.github.gameMakerTool;
-
-public abstract class AbstractExceptionManager{
-	public abstract void catchException(Exception e);
-}
--- src/main/java/io/github/gameMakerTool/ExceptionManager.java
+++
@@ -1,8 +0,0 @@
-package io.github.gameMakerTool;
-
-public class ExceptionManager extends AbstractExceptionManager{
-	@Override
-	public void catchException(Exception e) {
-		System.out.println("Error:/ "+e.getMessage()+".");
-	}
-}
