331c58e2aba3b802e74059e35ea344ed51f39e04
TomasJaures <t.jaures01@ufromail.cl>
2025-01-03 19:47:18 -0400

    Añadido Clase TileSimple
    clase dedicada a ser un clon mas simple de "Tile"
    
    Objetivo: Ser compatible con el json

---
+++ src/main/java/io/github/presets/TileSimple.java
@@ -0,0 +1,24 @@
+package io.github.levelMaker;
+
+public class TileSimple {
+
+    private int unitX;
+    private int unitY;
+    private float width;
+    private float height;
+    private boolean solid;
+    private int layer;
+    private int idTextura;
+    private String tipoBloque;
+
+    TileSimple(int unitX, int unitY,float width, float height, boolean solid, int layer, int idTextura, String tipoBloque){
+        this.unitX = unitX;
+        this.unitY = unitY;
+        this.width = width;
+        this.height = height;
+        this.solid = solid;
+        this.layer = layer;
+        this.idTextura = idTextura;
+        this.tipoBloque = tipoBloque;
+    }
+}
